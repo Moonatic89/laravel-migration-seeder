738ebe9d1fbad1b0c19459670b19183700385eaf
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2022-01-14 18:34:50 +0100

    TEST - Adding style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,26 @@
 
     @foreach ($flights as $flight)
 
-    <div class="flightCard">
+    <div class="flightCard d_flex">
 
         <div class="destination">
             <h3>Destinazione</h3>
             {{$flight->destination}}
+        </div>
+
+        <div class="airline">
+            <h3>Linea Aerea</h3>
+            {{$flight->airline}}
+        </div>
+
+        <div class="departure">
+            <h3>Partenza il:</h3>
+            {{$flight->departure}}
+        </div>
+
+        <div class="arrival">
+            <h3>Arrivo il:</h3>
+            {{$flight->arrival}}
         </div>
 
 
